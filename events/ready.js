f8992c734d378e89e3cecd42c94e78ccf21825c3
LucasCtrl <lucasalt@protonmail.com>
2018-12-21 17:07:10 +0100

    Fix bt status

--- events/ready.js
+++ events/ready.js
@@ -1,20 +1,20 @@
 module.exports = (bot) => {
   console.log(`Ready to serve ${bot.guilds.size} servers`)
 
-  bot.user.setActivity(`${bot.config.prefix}help`, { type: 'WATCHING' })
+  bot.user.setActivity(`${bot.config.prefix}help`, { type: 'STREAMING' })
     .catch(console.error)
 
   setInterval(game1 => {
-    bot.user.setActivity(`${bot.guilds.size.toLocaleString()} servers - ${bot.guilds.reduce((mem, g) => (mem += g.memberCount), 0)} users`, { type: 'WATCHING' })
+    bot.user.setActivity(`${bot.guilds.size.toLocaleString()} servers - ${bot.guilds.reduce((mem, g) => (mem += g.memberCount), 0)} users`, { type: 'STREAMING' })
       .catch(console.error)
     setTimeout(game2 => {
-      bot.user.setActivity(`${bot.config.prefix}help`, { type: 'WATCHING' })
+      bot.user.setActivity(`${bot.config.prefix}help`, { type: 'STREAMING' })
         .catch(console.error)
       setTimeout(game3 => {
-        bot.user.setActivity(`https://discord.gg/nEDcagb for support`, { type: 'WATCHING' })
+        bot.user.setActivity(`https://discord.gg/nEDcagb for support`, { type: 'STREAMING' })
           .catch(console.error)
         setTimeout(game4 => {
-          bot.user.setActivity(`V2 launch ! More powerful, consumes less`, { type: 'WATCHING' })
+          bot.user.setActivity(`V2 launch ! More powerful, consumes less`, { type: 'STREAMING' })
             .catch(console.error)
         }, 100000)
       }, 100000)
