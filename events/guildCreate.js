f877c312b8d063a031764d663524eb9faf1ed158
LucasCtrl <lucasalt@protonmail.com>
2019-01-05 10:35:12 +0100

    Update webhook logs

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,36 +1,4 @@
-module.exports = (bot, hook, guild) => {
+module.exports = (bot, privateHook, guild) => {
   // Send a message using the webhook
-  const embeds = [{
-    'color': 3983420,
-    'description': `Join **${guild.name}**`,
-    'fields': [
-      {
-        'name': '___',
-        'value': ':round_pushpin: **GUILD INFO**',
-        'inline': false
-      },
-      {
-        'name': 'Guild name',
-        'value': guild.name,
-        'inline': true
-      },
-      {
-        'name': 'Guild id',
-        'value': guild.id,
-        'inline': true
-      },
-      {
-        'name': 'Guild size',
-        'value': guild.memberCount,
-        'inline': true
-      },
-      {
-        'name': 'Guild owner',
-        'value': `**${guild.owner.user.tag}** ${guild.owner.id}`,
-        'inline': false
-      }
-    ]
-  }]
-  hook.send({ embeds })
-  console.log(guild)
+  privateHook.send('Join **' + guild.name + '** with ' + guild.memberCount + ' users')
 }
